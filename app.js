528da27887e05eba3dbb19c0d31f5943b29fc0c9
S Marsh <99958247+SMarsh-git@users.noreply.github.com>
2022-03-10 13:26:19 +0000

    Add files via upload
    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const textInput = document.getElementById(`textInput`)
+const textOutput = document.getElementById(`textOutput`)
+const vowels = [`a`, `e`, `i`, `o`, `u`];
+const ending = `ay`
+textInput.addEventListener(`keyup`, (y) => {
+    const input = y.target.value;
+    const words = input.split(` `);
+    const output = words.map((word) => translator(word)).join(` `);
+    textOutput.innerText = output;
+});
+
+const translator = (input) => {
+    if(vowels.includes(input[0])) {
+        output = input + ending;
+    } else {
+        let consonants = "";
+        for(let i = 0; i < input.length; i++) {
+            if(vowels.includes(input[i])) {
+                break;
+            }
+            consonants += input[i];
+        }
+        output = input.substring(consonants.length) + consonants + ending;
+    }
+    return output;
+};
